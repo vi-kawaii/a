e7492e803a53764441fa0425761f7ab846c82621
vi__kawaii <71643953+vi-kawaii@users.noreply.github.com>
2024-02-03 01:15:18 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 import sys
 from flask import Flask
 from flask_discord_interactions import DiscordInteractions
-import os
 from dotenv import load_dotenv
+from textblob import TextBlob
+import os
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -16,7 +18,21 @@ app.config["DONT_REGISTER_WITH_DISCORD"] = True
 
 @discord.command()
 def a(ctx, text: str):
-    return f"You wrote {text}"
+    # Создаем объект TextBlob для анализа текста
+    blob = TextBlob(text)
+
+    # Получаем оценку настроения
+    sentiment_score = blob.sentiment.polarity
+
+    # Определяем настроение на основе оценки
+    if sentiment_score > 0:
+        mood = "positive"
+    elif sentiment_score < 0:
+        mood = "negative"
+    else:
+        mood = "neutral"
+
+    return f"You wrote: {text}\nSentiment Analysis: {mood}"
 
 discord.set_route("/interactions")
 discord.update_commands()
